0c3caeb5d82a69032267b0fd5594d6e6d778a122
Jared Mathes <j-mathes@users.noreply.github.com>
2018-03-20 08:11:01 -0600

    Form Letter Drill - 1
    Drill Step 1

---
+++ Chapter 3/FormLetter/FormLetter/FormLetter.cpp
@@ -0,0 +1,15 @@
+/*
+	Jared Mathes
+	2018-03-20
+	Chapter 3 Drill
+	Form Letter Program
+*/
+#include "../../../std_lib_facilities.h"
+
+int main()
+{
+	cout << "Please enter your first name (followed by 'enter'):\n";
+	string first_name;		// first_name is a variable of type string
+	cin >> first_name;		// read characters into first_name
+	cout << "Hello, " << first_name << "!\n";
+}
