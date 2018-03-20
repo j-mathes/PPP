b477b3023665a7bc6cb942a267166eafaf53eddf
Jared Mathes <j-mathes@users.noreply.github.com>
2018-03-20 08:28:20 -0600

    Form Letter Drill - 2
    Step 2

--- Chapter 3/FormLetter/FormLetter/FormLetter.cpp
+++ Chapter 3/FormLetter/FormLetter/FormLetter.cpp
@@ -2,14 +2,23 @@
 	Jared Mathes
 	2018-03-20
 	Chapter 3 Drill
-	Form Letter Program
+	Form Letter Program - Step 2
 */
 #include "../../../std_lib_facilities.h"
 
 int main()
 {
-	cout << "Please enter your first name (followed by 'enter'):\n";
+	cout << "Enter the name of the person you want to write to:\n";
 	string first_name;		// first_name is a variable of type string
 	cin >> first_name;		// read characters into first_name
-	cout << "Hello, " << first_name << "!\n";
+
+	cout << "\n";
+	cout << "Dear " << first_name << ",\n\n";
+
+	cout << "	How are you doing?  It was so good to hear from you the other day.\n";
+	cout << "We are all interested to hear what you have been up to.\n";
+	cout << "Please respond soon.\n\n";
+
+	keep_window_open();
+	return 0;
 }
