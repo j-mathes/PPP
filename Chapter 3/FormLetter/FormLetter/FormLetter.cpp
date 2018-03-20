feef343147a9f55fa7628dd3be833dabe60d7070
Jared Mathes <j-mathes@users.noreply.github.com>
2018-03-20 10:00:26 -0600

    Form Letter Drill - 4
    Step 4

--- Chapter 3/FormLetter/FormLetter/FormLetter.cpp
+++ Chapter 3/FormLetter/FormLetter/FormLetter.cpp
@@ -2,7 +2,7 @@
 	Jared Mathes
 	2018-03-20
 	Chapter 3 Drill
-	Form Letter Program - Step 3
+	Form Letter Program - Step 4
 */
 #include "../../../std_lib_facilities.h"
 
@@ -17,11 +17,28 @@ int main()
 	string friend_name;	
 	cin >> friend_name;	
 	cout << "\n";
+
+	cout << "If your friend is male, enter the letter M.\n";
+	cout << "If your friend is female, enter the letter F.\n";
+	char friend_sex = 0;
+	cin >> friend_sex;
 	
+	cout << "\n\n";
 	cout << "Dear " << first_name << ",\n\n";
 	cout << "	How are you doing?  It was so good to hear from you the other day.\n";
 	cout << "We are all interested to hear what you have been up to.\n\n";
 	cout << "Have you seen " << friend_name << " lately?\n\n";
+
+	if (friend_sex == 'm' || friend_sex == 'M')
+	{
+		cout << "If you see " << friend_name << ", please ask him to call me.\n";
+	}
+
+	if (friend_sex == 'f' || friend_sex == 'F')
+	{
+		cout << "If you see " << friend_name << ", please ask her to call me.\n";
+	}
+
 	cout << "Please respond soon.\n\n";
 
 	keep_window_open();
