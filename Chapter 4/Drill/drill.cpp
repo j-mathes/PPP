8f7c297cd9e61009cb7a4b158451c6406c5f0cef
Jared Mathes <j-mathes@users.noreply.github.com>
2018-03-26 13:51:13 -0600

    Chapter 4, Drill, Step 4

--- Chapter 4/Drill/drill.cpp
+++ Chapter 4/Drill/drill.cpp
@@ -1,17 +1,17 @@
 /*
 Jared Mathes
 2018-03-26
-Chapter 4-2
+Chapter 4-4
 Drill
 */
 #include "../../std_lib_facilities.h"
 
 int main()
 {
-	int number1{ 0 };
-	int number2{ 0 };
+	double number1{ 0.0 };
+	double number2{ 0.0 };
 	
-	cout << "Please enter two integers separated by a space. >";
+	cout << "Please enter two numbers separated by a space. >";
 	while (cin >> number1 >> number2)
 	{
 		if (number1 == number2)
@@ -20,14 +20,14 @@ int main()
 		} 
 		else
 		{
-			int smaller = number1 < number2 ? number1 : number2;
+			double smaller = number1 < number2 ? number1 : number2;
 			cout << "\nThe smaller value is: " << smaller << "\n";
 
-			int larger = number1 > number2 ? number1 : number2;
+			double larger = number1 > number2 ? number1 : number2;
 			cout << "\nThe larger value is: " << larger << "\n";
 		}
 
-		cout << "\nPlease enter two integers separated by a space. >";
+		cout << "\nPlease enter two numbers separated by a space. >";
 	}
 	
 	return 0;
