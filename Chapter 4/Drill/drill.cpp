91d33e4139c84421bdd522b8edc3da561b869874
Jared Mathes <j-mathes@users.noreply.github.com>
2018-03-26 13:42:07 -0600

    Chapter 4, Drill, Step 3

--- Chapter 4/Drill/drill.cpp
+++ Chapter 4/Drill/drill.cpp
@@ -14,12 +14,20 @@ int main()
 	cout << "Please enter two integers separated by a space. >";
 	while (cin >> number1 >> number2)
 	{
-		int smaller = number1 < number2 ? number1 : number2;
-		cout << "\n\nThe smaller value is :" << smaller;
+		if (number1 == number2)
+		{
+			cout << "\nThe numbers are equal!\n";
+		} 
+		else
+		{
+			int smaller = number1 < number2 ? number1 : number2;
+			cout << "\nThe smaller value is: " << smaller << "\n";
 
-		int larger = number1 > number2 ? number1 : number2;
-		cout << "\nThe larger value is :" << larger;
-		cout << "\n\nPlease enter two integers separated by a space. >";
+			int larger = number1 > number2 ? number1 : number2;
+			cout << "\nThe larger value is: " << larger << "\n";
+		}
+
+		cout << "\nPlease enter two integers separated by a space. >";
 	}
 	
 	return 0;
