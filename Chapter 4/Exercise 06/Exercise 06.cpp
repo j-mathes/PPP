9885e6f77f62f2b1a02e1eb9b93a5106dc68309e
Jared Mathes <jaredmathes@shaw.ca>
2018-04-04 17:52:33 -0600

    Exercise 06 finished.

--- Chapter 4/Exercise 06/Exercise 06.cpp
+++ Chapter 4/Exercise 06/Exercise 06.cpp
@@ -3,11 +3,83 @@ Jared Mathes
 2018-04-04
 Chapter 4
 Exercise 06
-Convert digits to words
+Convert digits to words - No error checking.  Will crash on wrong input.
 */
 #include "../../std_lib_facilities.h"
 
+vector<string> literal_number;
+
+void initialize_numbers()
+{
+	literal_number.push_back("zero");
+	literal_number.push_back("one");
+	literal_number.push_back("two");
+	literal_number.push_back("three");
+	literal_number.push_back("four");
+	literal_number.push_back("five");
+	literal_number.push_back("six");
+	literal_number.push_back("seven");
+	literal_number.push_back("eight");
+	literal_number.push_back("nine");
+}
+
+int get_number()
+{
+	const int not_a_symbol = literal_number.size();	// not_a_symbol is a value that does not correspond
+													// to a string in the numbers vector
+	int val = not_a_symbol;
+	if (cin >> val) return val;						// try to read an integer composed of digits
+
+	cin.clear();									// clear string after failed attempt to read an integer
+	string s;
+	cin >> s;
+	for (int i = 0; i < literal_number.size(); ++i)	// see if the string is in numbers
+		if (literal_number[i] == s) val = i;
+	if (val == not_a_symbol) error("unexpected number string: ", s);
+
+	return val;
+}
+
 int main()
 {
+	initialize_numbers();
+
+	bool is_digit{ true };
+	int output_value{ 0 };
+
+	while (true)
+	{
+		cout << "\nEither enter a digit or enter the word for the digit (i.e. '7' or 'seven').\n";
+		cout << "> ";
+
+		const int not_a_symbol = literal_number.size();
+		int val = not_a_symbol;
+		if (cin >> val)
+		{
+			is_digit = true;
+		}
+		else
+		{
+			cin.clear();
+			string s;
+			cin >> s;
+			for (int i = 0; i < literal_number.size(); ++i)
+				if (literal_number[i] == s) val = i;
+			if (val == not_a_symbol) error("unexpected number string: ", s);
+			is_digit = false;
+		}
+		output_value = val;
+
+		switch (is_digit)
+		{
+		case false:
+			cout << val << '\n';
+			break;
+		case true:
+			cout << literal_number[val] << '\n';
+			break;
+		}
+	}
+
 	return 0;
 }
