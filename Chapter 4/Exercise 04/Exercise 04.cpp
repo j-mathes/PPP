40d6d15ae2ce53bec6faf631c0e5dca0ace51963
Jared Mathes <jaredmathes@shaw.ca>
2018-04-03 23:17:07 -0600

    Exercise 4 completed

--- Chapter 4/Exercise 04/Exercise 04.cpp
+++ Chapter 4/Exercise 04/Exercise 04.cpp
@@ -9,25 +9,87 @@ Guessing Game
 
 int main()
 {
+	int low_range{ 1 };
+	int high_range{ 100 };
 	int number_to_guess{ 0 };
 	bool is_valid_entry{ false };
 
 	while (!is_valid_entry)
 	{
-		cout << "Enter a number from 1 to 100. >";
+		cout << "Enter a number from " << low_range << " to " << high_range << ". > ";
 		cin >> number_to_guess;
 
-		if (number_to_guess > 0 && number_to_guess < 101)
+		if (number_to_guess > (low_range - 1) && number_to_guess < (high_range + 1))
 		{
 			is_valid_entry = true;
+			cout << '\n';
 		}
 		else
 		{
 			cout << '\n' << "Not a valid entry, please try again." << '\n';
 		}
-
 	}
 
+	int counter{ 1 };
+	bool correct{ false };
+	char answer;
+
+	while (!correct)
+	{
+		int median = (low_range + high_range) / 2;
+		cout << "For each attempt, answer if the guess is (c)orrect, (l)ower, or (h)igher.\n";
+		cout << "Guess number " << counter << ". Is your number " << median << "? > ";
+		cin >> answer;
+		
+		switch (answer)
+		{
+		case 'c':
+		case 'C':
+			if (number_to_guess == median)
+			{
+				cout << "\nGuessed the number " << number_to_guess << " in " << counter << " guess" << ((counter == 1) ? "" : "es") << "!\n";
+				correct = true;
+			} 
+			else
+			{
+				cout << "Liar!\n\n";
+			}
+			break;
+
+		case 'l':
+		case 'L':
+			if (number_to_guess < median)
+			{
+				high_range = median;
+				cout << "\n\n";
+				++counter;
+			} 
+			else
+			{
+				cout << "Liar!\n\n";
+			}
+			break;
+
+		case 'h':
+		case 'H':
+			if (number_to_guess > median)
+			{			
+				low_range = median;
+				cout << "\n\n";
+				++counter;
+			} 
+			else
+			{
+				cout << "Liar!\n\n";
+			}
+			break;
+
+		default:
+			cout << "\nThat's not a valid entry.  Try again.\n\n";
+			--counter;
+			break;
+		}
+	}
 
 	return 0;
 }
