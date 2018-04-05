1b662d8e5d6020d1adc655f063fd0e8cfb5e8962
Jared Mathes <jaredmathes@shaw.ca>
2018-04-05 17:16:49 -0600

    Exercise 11 completed

--- Chapter 4/Exercise 11/Exercise 11.cpp
+++ Chapter 4/Exercise 11/Exercise 11.cpp
@@ -6,7 +6,31 @@
 
 #include "../../std_lib_facilities.h"
 
+vector<int> primes;
+
 int main()
 {
+	primes.push_back(2);
+	bool is_prime;
+
+	for (int i=3; i<=100; ++i)
+	{
+		is_prime = true;
+		for (int x : primes)
+		{
+			if (i % x == 0)
+			{
+				is_prime = false;
+				break;
+			}
+		}
+		if (is_prime)
+			primes.push_back(i);
+	}
+
+	for (int x : primes)
+	{
+		cout << x << '\n';
+	}
 	return 0;
 }
