12e1c43eb0a170a12e4c94599e6a4f5fe6e95807
Jared Mathes <jaredmathes@shaw.ca>
2018-04-09 15:06:15 -0600

    Drill - Step 0

---
+++ Chapter 5/Drill/Drill.cpp
@@ -0,0 +1,23 @@
+// Jared Mathes
+// 2018-04-09
+// Chapter 5
+// Drill
+// Step 0
+
+#include "../../std_lib_facilities.h"
+int main()
+try {
+	<< your code here >>
+		keep_window_open();
+	return 0;
+}
+catch (exception& e) {
+	cerr << "error: " << e.what() << '\n';
+	keep_window_open();
+	return 1;
+}
+catch (...) {
+	cerr << "Oops: unknown exception!\n";
+	keep_window_open();
+	return 2;
+}
