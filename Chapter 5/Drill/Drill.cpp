cb088e2c4c9430731545050535ab30dcf6e60e03
Jared Mathes <jaredmathes@shaw.ca>
2018-04-10 10:39:41 -0600

    Drill - Step 19 - type of vector not defined

--- Chapter 5/Drill/Drill.cpp
+++ Chapter 5/Drill/Drill.cpp
@@ -2,15 +2,15 @@
 // 2018-04-10
 // Chapter 5
 // Drill
-// Step 18
-// technically runs, but iterator should count up to the size of the string
+// Step 19
+// type of vector not defined
 
 #include "../../std_lib_facilities.h"
 int main()
 try {
-	string s = "Success!\n"; 
-	for (int i = 0; i<s.size(); ++i) 
-		cout << s[i];
+	vector<int> v(5); 
+	for (int i = 0; i <= v.size(); ++i); 
+	cout << "Success!\n";
 	keep_window_open();
 	return 0;
 }
