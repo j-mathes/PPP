297c506961704b86856bab69bd8df00cd7f1fb2b
Jared Mathes <jaredmathes@shaw.ca>
2018-04-10 11:00:35 -0600

    Drill - Step 22 - string type not compatible with char template - changed test expression and iterator max limit

--- Chapter 5/Drill/Drill.cpp
+++ Chapter 5/Drill/Drill.cpp
@@ -2,16 +2,16 @@
 // 2018-04-10
 // Chapter 5
 // Drill
-// Step 21
-// changed math expressions to produce output
+// Step 22
+// string type not compatible with char template
+// changed test expression and iterator max limit
 
 #include "../../std_lib_facilities.h"
 int main()
 try {
-	int x = 2; 
-	double d = 5.0; 
-	if (d == 2 * (x + 0.5)) 
-		cout << "Success!\n";
+	string s = "Success!\n"; 
+	for (int i = 0; i < s.size(); ++i) 
+		cout << s[i];
 	keep_window_open();
 	return 0;
 }
