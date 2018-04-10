9b2e355777c023cba47adbf919af8265e7673c87
Jared Mathes <jaredmathes@shaw.ca>
2018-04-10 09:50:13 -0600

    Drill - Step 14 - no errors

--- Chapter 5/Drill/Drill.cpp
+++ Chapter 5/Drill/Drill.cpp
@@ -2,15 +2,15 @@
 // 2018-04-10
 // Chapter 5
 // Drill
-// Step 13
-// for loop test expression should compare iterator to size
+// Step 14
+// no errors
 
 #include "../../std_lib_facilities.h"
 int main()
 try {
 	vector<char> v(5); 
-	for (int i = 0; i < v.size(); ++i); 
-	cout << "Success!\n";
+	for (int i = 0; i <= v.size(); ++i); 
+	cout << "Success!\n";;
 	keep_window_open();
 	return 0;
 }
