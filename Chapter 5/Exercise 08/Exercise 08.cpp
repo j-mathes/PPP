3efb28a0c12e811bddd0fa3787aa773a00e37e93
Jared Mathes <jaredmathes@shaw.ca>
2018-04-26 14:03:19 -0600

    Exercise 08

--- Chapter 5/Exercise 08/Exercise 08.cpp
+++ Chapter 5/Exercise 08/Exercise 08.cpp
@@ -8,5 +8,36 @@
 
 int main()
 {
+	cout << "Enter the number of values you want to sum: ";
 
+	int number_to_sum;
+	cin >> number_to_sum;
+
+	vector<int> values;
+
+	cout << "\nPlease enter some integers (press | to stop): ";
+
+	for (int value; cin >> value;)
+	{
+		values.push_back(value);
+	}
+
+	int sum = 0;
+
+	if (values.size() < number_to_sum)
+	{
+		cout << "\nYou didn't enter in enough integers to sum.\n";
+	} 
+	else
+	{
+		cout << "\nThe sum of the first " << number_to_sum << " numbers ( ";
+		for (int i = 0; i < number_to_sum; ++i)
+		{
+			sum += values[i];
+			cout << values[i] << " ";
+		}
+		cout << ") is " << sum << ".\n";
+	}
+
+	return 0;
 }
