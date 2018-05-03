e0ce1b4f3dbb97375f97e8090ec483fcf8182c3d
Jared Mathes <jaredmathes@shaw.ca>
2018-05-03 14:59:12 -0600

    Exercise 14 - WIP

--- Chapter 5/Exercise 14/Exercise 14.cpp
+++ Chapter 5/Exercise 14/Exercise 14.cpp
@@ -4,11 +4,28 @@
 // Exercise 14
 // Days of the week and numbers
 
+// refer to chapter 4 exercise 7 for a way to get day of the week
+
 #include "../../std_lib_facilities.h"
 
 int main()
 {
+	vector<int> sunday;
+	vector<int> monday;
+	vector<int> tuesday;
+	vector<int> wednesday;
+	vector<int> thursday;
+	vector<int> friday;
+	vector<int> saturday;
 
+	vector<int> rejected;
+
+	cout << "Enter a day of the week and an integer for that day.  Enter 'Stop' to end.\n\n";
+
+	while (true)
+	{
+		
+	}
 
 	return 0;
 }
