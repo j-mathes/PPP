254e02f915dd3440f7d43d8d420d10e29e18bff0
Jared Mathes <jaredmathes@shaw.ca>
2018-08-04 21:44:40 -0600

    Exercise 05

---
+++ Chapter 9/Exercise 05/Book.cpp
@@ -0,0 +1,19 @@
+// Jared Mathes
+// 2018-08-04
+// Chapter 9
+// Exercise 05
+// Library project
+// Book.cpp
+
+#include "Book.h"
+
+
+
+Book::Book()
+{
+}
+
+
+Book::~Book()
+{
+}
---
+++ Chapter 9/Exercise 05/Book.h
@@ -0,0 +1,17 @@
+// Jared Mathes
+// 2018-08-04
+// Chapter 9
+// Exercise 05
+// Library project
+// Book.h
+
+#pragma  once
+#include "../../std_lib_facilities.h"
+
+class Book
+{
+public:
+	Book();
+	~Book();
+};
+
