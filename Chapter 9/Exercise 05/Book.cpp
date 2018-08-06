75f28b56ce7f85c29461bd8f6157938435e8dc92
Jared Mathes <jaredmathes@shaw.ca>
2018-08-05 19:27:18 -0600

    Exercise 05

--- Chapter 9/Exercise 05/Book.cpp
+++ Chapter 9/Exercise 05/Book.cpp
@@ -5,15 +5,46 @@
 // Library project
 // Book.cpp
 
+#pragma once
 #include "Book.h"
+#include <regex>
 
-
-
-Book::Book()
+Book::Book(string isbn, string title, string author, Date copyright_date, bool is_checked_out)
+	: m_title(title), m_author(author), m_copyright_date(copyright_date), m_is_checked_out(is_checked_out)
 {
+	if (check_ISBN(isbn))
+		m_ISBN = isbn;
+	else
+		error("ISBN not in correct format");
+}
+
+void Book::check_in()
+{
+	if (is_checked_out())
+	{
+		m_is_checked_out = false;
+	} 
+	else
+	{
+		cout << "This book is already checked in." << endl;
+	}
 }
 
 
-Book::~Book()
+void Book::check_out()
 {
+	if (is_checked_out())
+	{
+		cout << "This book is already checked out." << endl;
+	}
+	else
+	{
+		m_is_checked_out = true;
+	}
+}
+
+bool Book::check_ISBN(const string& isbn)
+{
+	regex r("^\\d{9}[\\dX]$");
+	return regex_match(isbn, r);
 }
