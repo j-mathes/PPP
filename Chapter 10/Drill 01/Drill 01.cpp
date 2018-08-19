63629ff75963468239efe2a44ff314c02bf17022
Jared Mathes <jaredmathes@shaw.ca>
2018-08-19 15:39:43 -0600

    Drill 01

---
+++ Chapter 10/Drill 01/Drill 01.cpp
@@ -0,0 +1,30 @@
+// Jared Mathes
+// 2018-08-19
+// Chapter 10
+// Drill 01
+// Working with points
+
+#include "../../std_lib_facilities.h"
+
+struct Point
+{
+	int x;
+	int y;
+};
+
+int main()
+try
+{
+
+	return 0;
+}
+
+catch (exception& e)
+{
+	cerr << "exception: " << e.what() << endl;
+}
+
+catch (...)
+{
+	cerr << "exception\n";
+}
