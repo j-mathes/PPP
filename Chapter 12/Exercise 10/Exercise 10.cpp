68a0066c791694aa67229f434bb955eb85df005e
Jared Mathes <jaredmathes@shaw.ca>
2018-12-20 11:52:33 -0700

    Exercise 10

---
+++ Chapter 12/Exercise 10/Exercise 10.cpp
@@ -0,0 +1,45 @@
+//----------------------------------------------------------------------------
+// File:	Exercise 10.cpp
+// Date:	2018-12-20
+// Author:	JaredM
+//----------------------------------------------------------------------------
+// Solution:Chapter 12
+// Project:	Exercise 10
+//----------------------------------------------------------------------------
+// Description: File diagram from 12.8
+//----------------------------------------------------------------------------
+
+#include "../../std_lib_facilities.h"
+#include "../../Graph.h"
+#include "../../Simple_window.h"
+
+//----------------------------------------------------------------------------
+// main
+//----------------------------------------------------------------------------
+int main()
+try
+{
+	using namespace Graph_lib;
+
+	int tlX = 50;
+	int tlY = 50;
+	int winX = 800;
+	int winY = 800;
+
+	Point tl{ tlX,tlY };
+
+	Simple_window win{ tl,winX,winY,"Exercise 10" };
+
+
+	return 0;
+}
+
+catch (exception& e)
+{
+	cerr << "exception: " << e.what() << endl;
+}
+
+catch (...)
+{
+	cerr << "exception\n";
+}
